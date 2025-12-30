6875dda3ccb383edca15d1f68d860b20ea6bb11f
carlos1510 <carloscle1510@gmail.com>
2025-12-30 13:00:51 -0500

    add dinamic inserts values invoices

--- app/Services/SunatService.php
+++ app/Services/SunatService.php
@@ -27,81 +27,88 @@ class SunatService
         return $see;
     }
 
-    public function getInvoice()
+    public function getInvoice($data)
     {
         // Venta
         return (new Invoice())
-            ->setUblVersion('2.1')
-            ->setTipoOperacion('0101') // Venta - Catalog. 51
-            ->setTipoDoc('01') // Factura - Catalog. 01 
+            ->setUblVersion($data['ublVersion']) // UBL Version 2.1
+            ->setTipoOperacion($data['tipoOperacion']) // Venta - Catalog. 51
+            ->setTipoDoc($data['tipoDoc']) // Factura - Catalog. 01 
             ->setSerie('F001')
-            ->setCorrelativo('1')
-            ->setFechaEmision(new DateTime()) // Zona horaria: Lima
+            ->setCorrelativo($data['correlativo'])
+            ->setFechaEmision(new DateTime($data['fechaEmision'])) // Zona horaria: Lima
             ->setFormaPago(new FormaPagoContado()) // FormaPago: Contado
-            ->setTipoMoneda('PEN') // Sol - Catalog. 02
-            ->setCompany($this->getCompany())
-            ->setClient($this->getClient())
-            ->setMtoOperGravadas(100.00)
-            ->setMtoIGV(18.00)
-            ->setTotalImpuestos(18.00)
-            ->setValorVenta(100.00)
-            ->setSubTotal(118.00)
-            ->setMtoImpVenta(118.00)
-            ->setDetails($this->getDetails())
-            ->setLegends($this->getLegends());
+            ->setTipoMoneda($data['tipoMoneda']) // Sol - Catalog. 02
+            ->setCompany($this->getCompany($data['company']))
+            ->setClient($this->getClient($data['client']))
+            ->setMtoOperGravadas($data['mtoOperGravadas'])
+            ->setMtoIGV($data['mtoIGV'])
+            ->setTotalImpuestos($data['totalImpuestos'])
+            ->setValorVenta($data['valorVenta'])
+            ->setSubTotal($data['subTotal'])
+            ->setMtoImpVenta($data['mtoImpVenta'])
+            ->setDetails($this->getDetails($data['details']))
+            ->setLegends($this->getLegends($data['legends']));
     }
 
-    public function getCompany() {
+    public function getCompany($company) {
         return (new Company())
-            ->setRuc('20123456789')
-            ->setRazonSocial('GREEN SAC')
-            ->setNombreComercial('GREEN')
-            ->setAddress($this->getAddress());
+            ->setRuc($company['ruc'])
+            ->setRazonSocial($company['razonSocial'])
+            ->setNombreComercial($company['nombreComercial'])
+            ->setAddress($this->getAddress($company['address']));
     }
 
-    public function getClient() {
+    public function getClient($client) {
         return (new Client())
-            ->setTipoDoc('6')
-            ->setNumDoc('20000000001')
-            ->setRznSocial('EMPRESA X');
+            ->setTipoDoc($client['tipoDoc']) // DNI - Catalog. 06
+            ->setNumDoc($client['numDoc'])
+            ->setRznSocial($client['rznSocial']);
     }
 
-    public function getAddress() {
+    public function getAddress($address) {
         return (new Address())
-            ->setUbigueo('150101')
-            ->setDepartamento('LIMA')
-            ->setProvincia('LIMA')
-            ->setDistrito('LIMA')
-            ->setUrbanizacion('-')
-            ->setDireccion('Av. Villa Nueva 221')
-            ->setCodLocal('0000'); // Codigo de establecimiento asignado por SUNAT, 0000 por defecto.
+            ->setUbigueo($address['ubigueo'])
+            ->setDepartamento($address['departamento'])
+            ->setProvincia($address['provincia'])
+            ->setDistrito($address['distrito'])
+            ->setUrbanizacion($address['urbanizacion'])
+            ->setDireccion($address['direccion'])
+            ->setCodLocal($address['codLocal']); // Codigo de establecimiento asignado por SUNAT, 0000 por defecto.
     }
 
-    public function getDetails() {
-         $item = (new SaleDetail())
-            ->setCodProducto('P001')
-            ->setUnidad('NIU') // Unidad - Catalog. 03
-            ->setCantidad(2)
-            ->setMtoValorUnitario(50.00)
-            ->setDescripcion('PRODUCTO 1')
-            ->setMtoBaseIgv(100)
-            ->setPorcentajeIgv(18.00) // 18%
-            ->setIgv(18.00)
-            ->setTipAfeIgv('10') // Gravado Op. Onerosa - Catalog. 07
-            ->setTotalImpuestos(18.00) // Suma de impuestos en el detalle
-            ->setMtoValorVenta(100.00)
-            ->setMtoPrecioUnitario(59.00)
-            ;
+    public function getDetails($details) {
+        $green_details = [];
+
+        foreach ($details as $detail) {
+            $green_details[] = (new SaleDetail())
+                ->setTipAfeIgv($detail['tipAfeIgv'])
+                ->setCodProducto($detail['codProducto'])
+                ->setUnidad($detail['unidad'])
+                ->setCantidad($detail['cantidad'])
+                ->setMtoValorUnitario($detail['mtoValorUnitario'])
+                ->setDescripcion($detail['descripcion'])
+                ->setMtoBaseIgv($detail['mtoBaseIgv'])
+                ->setPorcentajeIgv($detail['porcentajeIgv'])
+                ->setIgv($detail['igv'])
+                ->setTotalImpuestos($detail['totalImpuestos'])
+                ->setMtoValorVenta($detail['mtoValorVenta'])
+                ->setMtoPrecioUnitario($detail['mtoPrecioUnitario']);
+        }
         
-        return [$item];
+        return $green_details;
     }
 
-    public function getLegends() {
-        $legend = (new Legend())
-            ->setCode('1000') // Monto en letras - Catalog. 52
-            ->setValue('SON DOSCIENTOS TREINTA Y SEIS CON 00/100 SOLES');
+    public function getLegends($legends) {
+        $green_legends = [];
+
+        foreach ($legends as $legend) {
+            $green_legends[] = (new Legend())
+                ->setCode($legend['code'])
+                ->setValue($legend['value']);
+        }
         
-        return [$legend];
+        return $green_legends;
     }
 
     public function sunatResponse($result) {
