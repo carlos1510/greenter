1538807c9bea3f6ee46f22a6544a3d79e420864b
carlos1510 <carloscle1510@gmail.com>
2025-12-30 10:50:03 -0500

    add class service sunat and refactoring invoice controller

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company;
-use DateTime;
+use App\Services\SunatService;
+
 use Greenter\Model\Client\Client;
 use Greenter\Model\Company\Address;
 use Greenter\Model\Company\Company as ModelCompany;
-use Greenter\Model\Sale\FormaPagos\FormaPagoContado;
-use Greenter\Model\Sale\Invoice;
+
 use Greenter\Model\Sale\Legend;
 use Greenter\Model\Sale\SaleDetail;
-use Greenter\See;
-use Greenter\Ws\Services\SunatEndpoints;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class InvoiceController extends Controller
@@ -24,76 +23,10 @@ class InvoiceController extends Controller
     {
         $company = Company::where('user_id', JWTAuth::user()->id)->first();
 
-        $certificate = Storage::get($company->cert_path);
-        // Lógica para enviar la factura
-        $see = new See();
-        $see->setCertificate($certificate);
-        $see->setService(SunatEndpoints::FE_BETA);
-        $see->setClaveSOL($company->ruc, $company->sol_user, $company->sol_pass);
+        $sunat = new SunatService();
 
-        // Cliente
-        $client = (new Client())
-            ->setTipoDoc('6')
-            ->setNumDoc('20000000001')
-            ->setRznSocial('EMPRESA X');
-
-        // Emisor
-        $address = (new Address())
-            ->setUbigueo('150101')
-            ->setDepartamento('LIMA')
-            ->setProvincia('LIMA')
-            ->setDistrito('LIMA')
-            ->setUrbanizacion('-')
-            ->setDireccion('Av. Villa Nueva 221')
-            ->setCodLocal('0000'); // Codigo de establecimiento asignado por SUNAT, 0000 por defecto.
-
-        $company = (new ModelCompany())
-            ->setRuc('20123456789')
-            ->setRazonSocial('GREEN SAC')
-            ->setNombreComercial('GREEN')
-            ->setAddress($address);
-
-        // Venta
-        $invoice = (new Invoice())
-            ->setUblVersion('2.1')
-            ->setTipoOperacion('0101') // Venta - Catalog. 51
-            ->setTipoDoc('01') // Factura - Catalog. 01 
-            ->setSerie('F001')
-            ->setCorrelativo('1')
-            ->setFechaEmision(new DateTime()) // Zona horaria: Lima
-            ->setFormaPago(new FormaPagoContado()) // FormaPago: Contado
-            ->setTipoMoneda('PEN') // Sol - Catalog. 02
-            ->setCompany($company)
-            ->setClient($client)
-            ->setMtoOperGravadas(100.00)
-            ->setMtoIGV(18.00)
-            ->setTotalImpuestos(18.00)
-            ->setValorVenta(100.00)
-            ->setSubTotal(118.00)
-            ->setMtoImpVenta(118.00)
-            ;
-
-        $item = (new SaleDetail())
-            ->setCodProducto('P001')
-            ->setUnidad('NIU') // Unidad - Catalog. 03
-            ->setCantidad(2)
-            ->setMtoValorUnitario(50.00)
-            ->setDescripcion('PRODUCTO 1')
-            ->setMtoBaseIgv(100)
-            ->setPorcentajeIgv(18.00) // 18%
-            ->setIgv(18.00)
-            ->setTipAfeIgv('10') // Gravado Op. Onerosa - Catalog. 07
-            ->setTotalImpuestos(18.00) // Suma de impuestos en el detalle
-            ->setMtoValorVenta(100.00)
-            ->setMtoPrecioUnitario(59.00)
-            ;
-
-        $legend = (new Legend())
-            ->setCode('1000') // Monto en letras - Catalog. 52
-            ->setValue('SON DOSCIENTOS TREINTA Y SEIS CON 00/100 SOLES');
-
-        $invoice->setDetails([$item])
-                ->setLegends([$legend]);
+        $see = $sunat->getSee($company);
+        $invoice = $sunat->getInvoice();
 
         $result = $see->send($invoice);
 
